1c11ab4ad82a120b6cae0342e285723dd6301379
Sarath Kumar <worksarath95@gmail.com>
2022-10-20 12:00:48 +0530

    CustomUser model created

---
+++ newpro/owner/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .managers import CustomUserManager
+
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
