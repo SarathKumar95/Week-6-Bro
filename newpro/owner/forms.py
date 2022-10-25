14b06e7582b7eac5c619ceb3c123bdb3045ed879
Sarath Kumar <worksarath95@gmail.com>
2022-10-25 10:53:00 +0530

    custom form validation creating errors. Fixed.

--- newpro/owner/forms.py
+++ newpro/owner/forms.py
@@ -4,30 +4,16 @@ from django.core.exceptions import ValidationError
 
 from .models import CustomUser
 
-from crispy_forms.helper import FormHelper
-
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email',)
 
-    def clean(self):
-        username = self.cleaned_data.get('username')
 
-        if username[0].isupper():
-            raise ValidationError('Usernames should start with small letters.')
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
         fields = ('username', 'email',)
-
-
-    def clean(self):
-        username = self.cleaned_data.get('username')
-
-        if username[0].isupper():
-            raise ValidationError('Usernames should start with small letters.')
-
